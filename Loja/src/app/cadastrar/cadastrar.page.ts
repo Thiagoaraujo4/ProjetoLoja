685f7521f40bd58ecd74de528ccc31be9cde2cbe
Thiago Araujo <thiagolinhares67@hotmail.com>
2019-12-10 10:27:03 -0300

    Criação da função DadosCadstrar no cadastrar.page.ts  para pega os dados do cadastrar.page.html para depois cadastrar no banco de dados

--- Loja/src/app/cadastrar/cadastrar.page.ts
+++ Loja/src/app/cadastrar/cadastrar.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CpuInfo } from 'os';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -8,7 +9,49 @@ import { CpuInfo } from 'os';
 })
 export class CadastrarPage implements OnInit {
 
-  constructor() { }
+   public model: DadosCadastrar = null;
+
+  constructor() {
+
+    this.model = new DadosCadastrar();
+
+    this.model.nome="";
+    this.model.cpf="";
+    this.model.telefone="";
+    this.model.email="";
+    this.model.logradouro="";
+    this.model.numero="";
+    this.model.complemento="";
+    this.model.bairro="";
+    this.model.cep="";
+    this.model.usuario="";
+    this.model.senha="";
+    this.model.foto="";
+  }
+
+  public efetuarcadastro(){
+
+    var headers = new HttpHeaders();
+    headers.append("Access-Control-Allow-Origin","*");
+    headers.append("Access-Control-Method","POST");
+    headers.append("Accept","application/json");
+    headers.append("Content-Type","application/json");
+
+    var dados={
+      "usuario":this.model.usuario,
+      "senha":this.model.senha,
+      "foto":this.model.foto,
+      "telefone":this.model.telefone,
+      "email":this.model.email,
+      "logradouro":this.model.logradouro,
+      "numero":this.model.numero,
+      "complemeto":this.model.complemento,
+      "bairro":this.model.bairro,
+      "cep":this.model.cep,
+      "nome":this.model.nome,
+      "cpf":this.model.cpf
+    }
+  }
 
   ngOnInit() {
   }
@@ -19,18 +62,18 @@ export class CadastrarPage implements OnInit {
 Criamos uma classe (DadosCadastrar) com os dados que vamos trabalhar. Que vai vim do cadastrar.page.html
 */
 
-export class DadosCadstrar{
+export class DadosCadastrar {
 
-  nome:string;
-  cpf:string;
-  telefone:string;
-  email:string;
-  logradouro:string;
-  numero:string;
-  complemento:string;
-  bairro:string;
-  cep:string;
-  usuario:string;
-  senha:string;
-  foto:string;
+  nome: string;
+  cpf: string;
+  telefone: string;
+  email: string;
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  cep: string;
+  usuario: string;
+  senha: string;
+  foto: string;
 }
