d26b7917e03d8361695c1d1196749ac724baf0dd
Thiago Araujo <thiagolinhares67@hotmail.com>
2019-12-10 09:39:17 -0300

    Criação de uma classe no cadastrar.page.ts para realizar as passagens do dados do cadastrar.page.html

--- Loja/src/app/cadastrar/cadastrar.page.ts
+++ Loja/src/app/cadastrar/cadastrar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CpuInfo } from 'os';
 
 @Component({
   selector: 'app-cadastrar',
@@ -12,4 +13,24 @@ export class CadastrarPage implements OnInit {
   ngOnInit() {
   }
 
+}
+
+/*
+Criamos uma classe (DadosCadastrar) com os dados que vamos trabalhar. Que vai vim do cadastrar.page.html
+*/
+
+export class DadosCadstrar{
+
+  nome:string;
+  cpf:string;
+  telefone:string;
+  email:string;
+  logradouro:string;
+  numero:string;
+  complemento:string;
+  bairro:string;
+  cep:string;
+  usuario:string;
+  senha:string;
+  foto:string;
 }
