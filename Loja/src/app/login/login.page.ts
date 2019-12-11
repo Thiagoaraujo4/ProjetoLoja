02ef8133a088b332d387ac7d05c8f7cca7958197
Thiago Araujo <thiagolinhares67@hotmail.com>
2019-12-11 10:24:24 -0300

    Criação do metodo que valida o login no login.page.ts

--- Loja/src/app/login/login.page.ts
+++ Loja/src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +9,51 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router:Router) { }
+  private url:string = "http://localhost/dbloja/data/usuario/login.php";
+  public modelUS:Usuario;
+
+  constructor(private router:Router, private http:HttpClient) {
+    this.modelUS.usuario="";
+    this.modelUS.senha="";
+
+   }
 
   public cadastrar(){
     this.router.navigate(['/cadastrar']);
   }
 
+  public efetuarlogin(){
+    var headers = new HttpHeaders();
+    headers.append("Accept","application/json");
+    headers.append("Content-Type","application/json");
+    headers.append("Access-Control-Allow-Origin","*");
+    
+    var dados ={
+      "usuario":this.modelUS.usuario,
+      "senha":this.modelUS.senha
+    };
+
+    this.http.get(this.url,{headers:headers,params:dados}).subscribe(
+      data=>{
+        console.log(data);
+      },
+      error=>{
+        console.log("Erro ao tentar logar"+error);
+      }
+
+      
+    );
+
+
+  }
+
   ngOnInit() {
   }
+
+}
+export class Usuario{
+  
+  usuario:string;
+  senha:string;
 
 }
